45c230235bff7f2ad130bc7c783ec5a61f1c0e72
shamlin143 <shamlin@gvtc.com>
2020-06-27 12:46:12 -0500

    Signup Page

--- routes/api/signup.js
+++ routes/api/signup.js
@@ -9,11 +9,11 @@ signup
   .get(booksController.findAll)
   .post(booksController.create);
 
-// Matches with "/api/books/:id"
-router
-  .route("/:id")
-  // .get(booksController.findById)
-  // .put(booksController.update)
-  // .delete(booksController.remove);
+// // Matches with "/api/books/:id"
+// router
+//   .route("/:id")
+//   // .get(booksController.findById)
+//   // .put(booksController.update)
+//   // .delete(booksController.remove);
 
 module.exports = router;
